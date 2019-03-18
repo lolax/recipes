e16cae169fef5c8658030702c28658e43d1d2386
Lola <lolahef@gmail.com>
2019-03-17 21:19:36 -0400

    Setup firebase

--- recipes/src/main.js
+++ recipes/src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import config from './config'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+firebase.initializeApp(config)
 
 new Vue({
   router,
