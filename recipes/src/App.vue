59ac564cfb5aa81d721fea865a2912d14f8ac2ba
Lola <lolahef@gmail.com>
2019-03-17 21:19:26 -0400

    Added logout

--- recipes/src/App.vue
+++ recipes/src/App.vue
@@ -1,45 +1,95 @@
 <template>
   <div id="app">
-    <div class="nav">
-      <router-link class="link" to="/">Home</router-link>
+    <div v-if="validated" class="nav">
       <router-link class="link" to="/recipes">Recipes</router-link>
-      <router-link class="link" to="/new">New Recipe</router-link>
+      <router-link class="link" to="/new">New</router-link>
+      <div class="link" @click="logout">Logout</div>
     </div>
     <router-view/>
   </div>
 </template>
 
+<script>
+  import firebase from 'firebase'
+  require('firebase/auth')
+
+  export default {
+    name: "app",
+    data() {
+      return {
+        validated: false,
+      }
+    },
+    mounted() {
+      if (localStorage.getItem('token')) {
+        this.validated = true
+      }
+    },
+    methods: {
+      logout() {
+        firebase
+        .auth()
+        .signOut()
+        .then(
+          () => {
+            this.$router.push("/")
+            this.validated = false
+            localStorage.clear()
+          },
+          () => alert(err.message)
+        )
+      },
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  height: 100vh;
-  margin: 0;
-}
-.nav {
-  width: 100%;
-  background: lavender;
-  padding: 40px 0;
-}
-.link {
-  display: inline-block;
-  width: 100px;
-  margin: 0 20px;
-  border: 1px solid lavender;
-  text-decoration: none;
-  color: black;
-  background: white;
-  padding: 15px;
-  outline: 0;
-  outline-offset: 0;
-  transition: 200ms;
-}
-.link:hover {
-  border: 1px solid black;
-  outline: 1px solid black;
-  outline-offset: 5px;
-  transition: 200ms;
-}
+  body {
+    background: #3C5A95;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    margin: 0;
+  }
+  .nav {
+    width: 100%;
+    padding: 40px 0;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+  }
+  .link {
+    width: 50px;
+    margin: 0 20px;
+    border: 1px solid #3C5A95;
+    text-decoration: none;
+    color: white;
+    background: transparent;
+    padding: 15px;
+    -webkit-box-shadow: 0;
+    -moz-box-shadow: 0;
+    box-shadow: 0;
+    cursor: pointer;
+  }
+  .link:hover {
+    background: white;
+    color: black;
+    border: 1px solid black;
+    -webkit-box-shadow: 0 0 1px 1px lightgrey;
+    -moz-box-shadow: 0 0 1px 1px lightgrey;
+    box-shadow: 0 0 1px 1px lightgrey;
+  }
+  @media (max-width: 500px) {
+    .nav {
+      width: 90%;
+      margin: auto;
+    }
+    .link {
+      margin: 0 10px;
+    }
+  }
 </style>
