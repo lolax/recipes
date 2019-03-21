dcfb7a48fb3507c76f0164f268f993aa85729a49
Lola <lolahef@gmail.com>
2019-03-21 00:07:43 -0400

    Setting up knex for production

--- knexfile.js
+++ knexfile.js
@@ -31,19 +31,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    debug: true,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      tableName: './data/migrations'
+    },
+    ssl: true,
+    useNullAsDefault: true
   }
 
 };
