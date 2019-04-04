7dcfc1c2f588e2d300e2f11dcdb46fce4e07e524
Lola <lolahef@gmail.com>
2019-04-03 20:14:49 -0400

    Updated config

--- recipes/src/main.js
+++ recipes/src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import config from './config'
 import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: process.env.VUE_APP_apiKey.trim(),
+  authDomain: process.env.VUE_APP_authDomain.trim(),
+  databaseURL: process.env.VUE_APP_databaseURL.trim(),
+  projectId: process.env.VUE_APP_projectId.trim(),
+  storageBucket: process.env.VUE_APP_storageBucket.trim(),
+  messagingSenderId: process.env.VUE_APP_messagingSenderId.trim()
+};
 
 firebase.initializeApp(config)
 
